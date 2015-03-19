08814d18eca0cd588c2137b30338751ce393a3ce
BurnzZ <kevinoxy@gmail.com>
2015-03-20 00:31:28 +0800

    fetching and building photo URL from Flickr API now works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,12 @@
+// freewall global
 var wall;
+
+var api_key = 'd9edd8af4c7623dd292b6d5963dcd6d2';
+
+var url_base = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key;
+var url_params = "&format=json&nojsoncallback=1&per_page=10";
+
+
 
 /* Called after featured-image has been loaded */
 function afterWallBrickImageLoad() {
@@ -45,13 +53,49 @@ function generateWall(wall) {
 	afterWallBrickImageLoad();
 }
 
-$('document').ready(function() {
+function loadWall() {
 
-	var api_key = 'd9edd8af4c7623dd292b6d5963dcd6d2';
-	var api_secret = 'ec0eeb164369fb60';
+}
 
+function buildWall() {
 	// init the wall
 	wall = new freewall("#results");
 	generateWall(wall);
 	wall.refresh();
+}
+
+$('document').ready(function() {
+
+	$('#btn-submit').click(function(event) {
+		event.preventDefault();
+
+		var query = "&text=" + $("#query").val();
+		var URL = url_base + url_params + query;
+		console.log(URL);
+
+		$.getJSON(URL, function(result) {
+			var images = result.photos;
+			console.log(images);
+
+			images.photo.forEach(function(unit) {
+				var photoUrl = "https://farm" + unit.farm + ".staticflickr.com/" + unit.server + "/" + unit.id + "_" + unit.secret + ".jpg";	
+			});
+
+			// var photo = result.photos.photo[0];
+			// console.log(photo);
+
+			// console.log(photoUrl);
+		});
+	});
+
+	buildWall();
 });
+
+
+
+
+
+
+
+
+
