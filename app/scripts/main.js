47ae9e3166e45066907ec5731cec932557425257
BurnzZ <kevinoxy@gmail.com>
2015-03-20 02:51:05 +0800

    attempted to make background image with photo of the day

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,11 @@ var api_key = 'd9edd8af4c7623dd292b6d5963dcd6d2';
 var url_base = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + api_key;
 var url_params = "&format=json&nojsoncallback=1&per_page=10";
 
+var url_interestingness = "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=" + api_key;
+var url_interestingness_params = "&per_page=1&format=json&nojsoncallback=1"
+
+var url_getSizes = "http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&per_page=1&api_key=" + api_key;
+
 
 
 /* Called after featured-image has been loaded */
@@ -79,14 +84,41 @@ function loadWall(page) {
 	buildWall();
 }
 
+/* Loads the first result of Flickr's most interesting photo of the day */
+function buildHeader() {
+
+	$.getJSON(url_interestingness + url_interestingness_params, function(result) {
+
+		var img = result.photos.photo[0];
+
+		var title = img.title;
+		// $('#photo-of-the-day').html('Photo of the Day: <a href="' + url + '">' + title + '</a>');
+
+		// another AJAX in order to get original img 
+		$.getJSON(url_getSizes + '&photo_id=' + img.id, function(result) {
+
+			var url = result.sizes.size[result.sizes.size.length-1].source;
+			console.log(url);
+			$('#main').css({'background' : 'url(' + url + ') no-repeat center center fixed'});
+		});	
+
+	
+	
+	});
+}
+
 function buildWall() {
+
 	// init the wall
 	wall = new freewall("#results");
+
 	generateWall(wall);
 	wall.refresh();
 }
 
 $('document').ready(function() {
+
+	buildHeader();
 
 	$('#btn-submit').click(function(event) {
 		event.preventDefault();
