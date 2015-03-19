92cd2fb952ee7801afab0f2d14f755c89d472769
BurnzZ <kevinoxy@gmail.com>
2015-03-20 01:06:02 +0800

    building of freewall from photoURLs now works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,8 +53,30 @@ function generateWall(wall) {
 	afterWallBrickImageLoad();
 }
 
-function loadWall() {
+function loadWall(page) {
+	console.log(page);
+	page.forEach(function(unit) {
 
+		$('#results').append(
+			'<a href="' + unit.url + '" title="">' +
+			    '<div class="brick">' +
+
+			        '<div class="loading-gif">' +
+			            '<img src="../images/loader.gif">' +
+			        '</div>' +
+
+			        '<div class="tiled-wrapper">' +
+			            '<img src="' + unit.url + '">' +
+			        '</div>' +
+
+			        '<div class="result-title">' +
+			            unit.title +
+			        '</div>' +
+			    '</div>' +
+			'</a>');
+	});
+
+	buildWall();
 }
 
 function buildWall() {
@@ -69,22 +91,28 @@ $('document').ready(function() {
 	$('#btn-submit').click(function(event) {
 		event.preventDefault();
 
+		// clears prev results
+		$('#results').html("");
+
+		// builds the query to the API: flickr.photos.search
 		var query = "&text=" + $("#query").val();
 		var URL = url_base + url_params + query;
-		console.log(URL);
 
 		$.getJSON(URL, function(result) {
 			var images = result.photos;
 			console.log(images);
 
+			var page = new Array();
+
 			images.photo.forEach(function(unit) {
-				var photoUrl = "https://farm" + unit.farm + ".staticflickr.com/" + unit.server + "/" + unit.id + "_" + unit.secret + ".jpg";	
+				var obj = {}
+				obj.url = "https://farm" + unit.farm + ".staticflickr.com/" + unit.server + "/" + unit.id + "_" + unit.secret + ".jpg";	
+				obj.title = unit.title
+
+				page[page.length] = obj
 			});
 
-			// var photo = result.photos.photo[0];
-			// console.log(photo);
-
-			// console.log(photoUrl);
+			loadWall(page);
 		});
 	});
 
